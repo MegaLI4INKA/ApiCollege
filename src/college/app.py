1972fa3939a57c948a843354d159a9a22c49c266
MegaLI4INKA <Denis.shalaev2019@yandex.ru>
2022-10-18 17:50:59 +0300

    sd

--- src/college/app.py
+++ src/college/app.py
@@ -20,28 +20,36 @@ def get_db():
         db.close()
 
 
-@app.get("/api/persons/groupandperson/name/{name}")
-def get_people(name:str,db: Session = Depends(get_db)):
-    persona = db.query(person).filter(person.Firstname == name).first()
-    return PersonAndGroup(Firstname=persona.Firstname,GroupName=persona.PersonGroup.GroupName,
-                          LastName=persona.LastName,MiddleName=persona.MiddleName,Number=persona.Numbers)
-
-
+# @app.get("/api/persons/groupandperson/name/{name}")
+# def get_people(name: str, db: Session = Depends(get_db)):
+#     persona = db.query(person).filter(person.Firstname == name).first()
+#     return PersonAndGroup(Firstname=persona.Firstname, GroupName=persona.PersonGroup.GroupName,
+#                           LastName=persona.LastName, MiddleName=persona.MiddleName, Number=persona.Numbers)
+#
 
 # ---------------------------------Persons------------------------------------
-@app.get("/api/persons")
-def get_people(db: Session = Depends(get_db)):
-    persons = db.query(person).all()
-    return persons
+# @app.get("/api/persons")
+# def get_people(db: Session = Depends(get_db)):
+#     persons = db.query(person).all()
+#     return persons
+
 
 # Получает всех людей по id группы
-@app.get("/api/persons/groupid/{id}")
-def get_people(id: int, db: Session = Depends(get_db)):
-    first = db.query(person).filter(person.Groupid == id).all()
-    return first
+# @app.get("/api/persons/groupid/{id}")
+# def get_people(id: int, db: Session = Depends(get_db)):
+#     person = db.query(person).filter(person.Groupid == id).all()
+#     return person
+
 
 # Получает всех людей по имени
-@app.get("/api/persons/name/{name}")
-def get_people(name: str,lastname:str=None, db: Session = Depends(get_db)):
-    first = db.query(person).filter(person.Firstname == name).all()
-    return first
+# @app.get("/Bazara")
+# def get_people(db: Session = Depends(get_db)):
+#     role = db.query(Role).where(Role.IdRole ==2)
+#     changerole = db.scalars(role).one()
+#     changerole.
+#     return 0
+
+@app.get("/api/getRaspisanie/id/{id}")
+def get_people(id:int, db: Session = Depends(get_db)):
+    raspisaniesall = db.query(Timetable).filter(Timetable.Idgroup == id).all()
+    return raspisaniesall
--- src/college/databases.py
+++ src/college/databases.py
@@ -1,71 +1,93 @@
+from importlib.metadata import metadata
+
 from fastapi import FastAPI
 from mysqlx import Column
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base, relationship
 from sqlalchemy.orm import Session
 from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
+from sqlalchemy.testing.schema import Table
 
-SQLALCHEMY_DATABASE_URL = "mssql://User:123@DESKTOP-M0LHBL5/testfastapi?driver=ODBC Driver 17 for SQL Server"
-# SQLALCHEMY_DATABASE_URL = "mysql+mysqldb://fionitos:Cvfhnajy201*@196.254.176.13:3306/db_project"
+# SQLALCHEMY_DATABASE_URL = "mssql://User:123@DESKTOP-M0LHBL5/testfastapi?driver=ODBC Driver 17 for SQL Server"
+SQLALCHEMY_DATABASE_URL = "mysql+mysqldb://fionitos:Cvfhnajy201*@196.254.176.13:3306/db_project"
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 Base = declarative_base()
 
 
 # ------------------------------------------------------------------------------------------------------------------------------------------------------
 # так как при запихивании этого в отдельный файл я пока оставлю это тут)
-class person(Base):
-    __tablename__ = "person"
+class Cvalification(Base):
+    __tablename__ = 'cvalification'
 
-    Personid = Column(Integer, primary_key=True, index=True)
-    LastName = Column(String)
-    Firstname = Column(String)
-    MiddleName = Column(String)
-    Numbers = Column(String)
-    Groupid = Column(Integer, ForeignKey("Groups.Groupid"))
+    idCvalification = Column(Integer, primary_key=True)
+    CvalificationName = Column(String(255), nullable=False)
 
 
-class Groups(Base):
-    __tablename__ = "Groups"
+class Group(Base):
+    __tablename__ = 'group'
 
-    Groupid = Column(Integer, primary_key=True, index=True)
-    GroupName = Column(String)
-
-    persons = relationship('person', backref='PersonGroup')
-
-
-class specialisation(Base):
-    __tablename__ = "specialisation"
-
-    IdSpecial = Column(Integer, primary_key=True, index=True)
-    SpecialName = Column(String)
-    Code = Column(String)
+    IdGroup = Column(Integer, primary_key=True)
+    IdPerson = Column(ForeignKey('person.IdPerson'), nullable=False, index=True)
+    GroupName = Column(String(255), nullable=False)
+    IdSpec = Column(ForeignKey('specialisation.IdSpecial'), nullable=False, index=True)
 
 
-# class role(Base):
-#     __tablename__ = "role"
-#
-#     IdRole = Column(Integer, primary_key=True, index=True)
-#     RoleName = Column(String, unique=True)
+class Person(Base):
+    __tablename__ = 'person'
 
-# class roleperson(Base):
-#     __tablename__ = "roleperson"
-#
-#     IdRole = Column(Integer, primary_key=True, index=True)
-#     IdPerson = Column(Integer, primary_key=True, index=True)
-
-# class timetable(Base):
-#     __tablename__ = "timetable"
-#
-#     idRaspisanie = Column(Integer, primary_key=True, index=True)
-#     LessonNumber = Column(Integer)
-#     WeekDay = Column(String, unique=True)
+    IdPerson = Column(Integer, primary_key=True)
+    IdGroup = Column(ForeignKey('group.IdGroup'), index=True)
+    LastName = Column(String(255), nullable=False)
+    FirstName = Column(String(255), nullable=False)
+    MiddleName = Column(String(255), nullable=False)
+    Number = Column(String(255), nullable=False)
 
 
-# class predmet(Base):
-#     __tablename__ = "predmet"
-#
-#     idPredmet = Column(Integer, primary_key=True, index=True)
-#     PredmetName = Column(String)
+class Predmet(Base):
+    __tablename__ = 'predmet'
+
+    idPredmet = Column(Integer, primary_key=True)
+    PredmetName = Column(String(255), nullable=False)
+
+
+class Role(Base):
+    __tablename__ = 'role'
+
+    IdRole = Column(Integer, primary_key=True)
+    RoleName = Column(String(255), nullable=False)
+
+
+class predmetteachere(Base):
+    __tablename__ = 'predmetteacher'
+
+    Idperson = Column(Integer, ForeignKey('person.IdPerson'), primary_key=True, nullable=False),
+    IdPredmet = Column(Integer, ForeignKey('predmet.idPredmet'), primary_key=True, nullable=False)
+
+
+class roleprson(Base):
+    __tablename__ = 'roleprson'
+
+    IdRole = Column(Integer, ForeignKey('role.IdRole'), primary_key=True, nullable=False),
+    IdPerson = Column(Integer, ForeignKey('person.IdPerson'), primary_key=True, nullable=False)
+
+
+class Specialisation(Base):
+    __tablename__ = 'specialisation'
+
+    IdSpecial = Column(Integer, primary_key=True)
+    SpecialName = Column(String(255), nullable=False)
+    Code = Column(String(255), nullable=False)
+    IdCvalification = Column(ForeignKey('cvalification.idCvalification'), nullable=False, index=True)
+
+class Timetable(Base):
+    __tablename__ = 'timetable'
+
+    idRaspisanie = Column(Integer, primary_key=True)
+    PredmetId = Column(ForeignKey('predmet.idPredmet'), nullable=False, index=True)
+    Idgroup = Column(ForeignKey('group.IdGroup'), nullable=False, index=True)
+    LessonNumber = Column(Integer, nullable=False)
+    WeekDay = Column(String(255), nullable=False)
+    NumberRoom = Column(Integer)
 
 
 # ------------------------------------------------------------------------------------------------------------------------------------------------------
